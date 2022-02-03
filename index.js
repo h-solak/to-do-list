1785611dfeb6261e717daf48ea1d8927f62fa777
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2022-02-03 23:27:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let taskList = [];
-let counter= 0;
+let counter = 0;
 let ul = document.querySelector("ul");
 
 const addTask = () => {
@@ -10,20 +10,20 @@ const addTask = () => {
     //CURRENTLY BUGGED -- IF WE DELETE A TASK, WE CANNOT ADD THAT TASK AGAIN
     //TO FIX IT, YOU HAVE TO DELETE THAT TASK FROM THE TASKS ARRAY
     const checkInclude = () => {
-        for(let i=0; i<taskList.length-1;i++){
-            if((taskList[i]==taskList[taskList.length-1])){
-                document.querySelector("h5").innerHTML = "You already have that task!!!"
-		        document.querySelector("h5").classList.add("shake");
-                
-                return false
+        for (let i = 0; i < taskList.length - 1; i++) {
+            if ((taskList[i] == taskList[taskList.length - 1])) {
+                document.querySelector("h5").innerHTML = "You already have that task!!!";
+                document.querySelector("h5").classList.add("shake");
+
+                return false;
             }
         }
         document.querySelector("h5").classList.remove("shake");
-        document.querySelector("h5").innerHTML = ""
-        return true
+        document.querySelector("h5").innerHTML = "";
+        return true;
     }
 
-    if ((currentTask != "")&&(checkInclude())){
+    if ((currentTask != "") && (checkInclude())) {
         createTask(currentTask)
         document.querySelector("#input-value").value = ""; //empty inside
     }
@@ -31,50 +31,57 @@ const addTask = () => {
 
 const createTask = (currentTask) => {
     let li = document.createElement("li");
-	li.appendChild(document.createTextNode(currentTask)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
+    li.appendChild(document.createTextNode(currentTask)); //makes text from input field the li text
+    ul.appendChild(li); //adds li to ul
 
-    /*I am new to this concept, so I got the lines 38-59 from https://codepen.io/JohnPaulFich/pen/MXmzzM */
-	// because it's in the function, it only adds it for new items
-	function taskDone() {
-		li.classList.toggle("done");
-	}
-	li.addEventListener("click",taskDone);
+    /*I am new to this concept, so I got most of the lines between 38-59 from this link https://codepen.io/JohnPaulFich/pen/MXmzzM */
+    // because it's in the function, it only adds it for new items
+    function taskDone() {
+        li.classList.toggle("done");
+    }
+    li.addEventListener("click", taskDone);
 
-	// START ADD DELETE BUTTON
-	var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteTask);
+    //creating delete button
+    const deleteButton = document.createElement("i");
+    deleteButton.classList.add("fa-regular")
+    deleteButton.classList.add("fa-trash-can")
 
-    function deleteTask(){
-        //hiding the deleted task
-		li.classList.add("delete")
+    //adding the delete button to the task
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(deleteButton);
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteTask);
+
+    function deleteTask() {
+        //hiding the deleted task - not deleting, just hiding it
+        li.classList.add("delete")
+
         //removing the deleted task from the tasks array
-        let deletedItem = li.innerHTML.slice(0,-18) //getting task from task<button>X</button>
+        let deletedItem = li.innerHTML.slice(0, -18); //getting task from task<button>X</button>
         const index = taskList.indexOf(deletedItem);
         if (index > -1) {
             taskList.splice(index, 1);
         }
-	}
+    }
 
     counter++;
 }
 
+
+/* NOT RELATED TO THE APP */
 //DYNAMIC FAVICON FOR BOTH DARK THEME AND LIGHT THEME
 lightSchemeIcon = document.querySelector('link#light-scheme-icon');
 darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
 
 function onUpdate() {
-  if (matcher.matches) {
-    lightSchemeIcon.remove();
-    document.head.append(darkSchemeIcon);
-  } else {
-    document.head.append(lightSchemeIcon);
-    darkSchemeIcon.remove();
-  }
+    if (matcher.matches) {
+        lightSchemeIcon.remove();
+        document.head.append(darkSchemeIcon);
+    } else {
+        document.head.append(lightSchemeIcon);
+        darkSchemeIcon.remove();
+    }
 }
 matcher = window.matchMedia('(prefers-color-scheme: dark)');
 matcher.addListener(onUpdate);
 onUpdate();
-
