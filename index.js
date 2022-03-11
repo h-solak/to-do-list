4fa626d6816722acb03770198cd105402de208d9
h-solak <hasan23solak@gmail.com>
2022-03-11 16:15:39 +0300

    Minor UI changes

--- index.js
+++ index.js
@@ -1,69 +1,71 @@
 let taskList = [];
-let counter= 0;
+let counter = 0;
 let ul = document.querySelector("ul");
 
+$(document).ready(function () {
+  if (localStorage.getItem("taskItem")) {
+    // liste localstorage belleğinde varsa oku
+    document.getElementById("allTasks").innerHTML =
+      localStorage.getItem("taskItem");
+    document.getElementById("clear-all").style.display = "block";
+  } else {
+    document.getElementById("allTasks").innerHTML = "";
+    document.getElementById("clear-all").style.display = "none";
+  }
+});
+
 const addTask = () => {
-    const currentTask = document.querySelector("#input-value").value;
-    taskList[counter] = currentTask;
+  const currentTask = document.querySelector("#input-value").value.trim();
 
-    //a function that doesn't let the user to enter the same task again!
-    //CURRENTLY BUGGED -- IF WE DELETE A TASK, WE CANNOT ADD THAT TASK AGAIN
-    //TO FIX IT, YOU HAVE TO DELETE THAT TASK FROM THE TASKS ARRAY
-    const checkInclude = () => {
-        for(let i=0; i<taskList.length-1;i++){
-            if((taskList[i]==taskList[taskList.length-1])){
-                document.querySelector("h5").innerHTML = "You already have that task!!!"
-		        document.querySelector("h5").classList.add("shake");
-                
-                return false
-            }
-        }
-        document.querySelector("h5").classList.remove("shake");
-        document.querySelector("h5").innerHTML = ""
-        return true
-    }
-
-    if ((currentTask != "")&&(checkInclude())){
-        createTask(currentTask)
-        document.querySelector("#input-value").value = ""; //empty inside
-    }
-}
+  if (currentTask != "") {
+    taskList.push(currentTask);
+    document.querySelector("#input-value").value = ""; //empty inside
+    createTask(currentTask);
+    document.getElementById("clear-all").style.display = "block";
+  }
+};
 
 const createTask = (currentTask) => {
-    let li = document.createElement("li");
-	li.appendChild(document.createTextNode(currentTask)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
+  document.querySelector("ul").innerHTML +=
+    '<li onclick="taskDone(this)">' +
+    currentTask +
+    '<span onclick="deleteTask(this.parentNode)"><i class="fa-regular fa-trash-can"></i></span></li>';
 
-    /*I am new to this concept, so I got the lines 38-59 from https://codepen.io/JohnPaulFich/pen/MXmzzM */
-	// because it's in the function, it only adds it for new items
-	function taskDone() {
-		li.classList.toggle("done");
-	}
-	li.addEventListener("click",taskDone);
+  localStorage.setItem(
+    "taskItem",
+    document.getElementById("allTasks").innerHTML
+  );
+};
 
-	// START ADD DELETE BUTTON
-	var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteTask);
-
-    function deleteTask(){
-        //hiding the deleted task
-		li.classList.add("delete")
-        //removing the deleted task from the tasks array
-        let deletedItem = li.innerHTML.slice(0,-18) //getting task from task<button>X</button>
-        const index = taskList.indexOf(deletedItem);
-        if (index > -1) {
-            taskList.splice(index, 1);
-        }
-	}
-
-    counter++;
+function taskDone(id) {
+  id.classList.toggle("done");
+  localStorage.setItem(
+    "taskItem",
+    document.getElementById("allTasks").innerHTML
+  );
 }
 
+function deleteTask(id) {
+  document.querySelector("ul").removeChild(id);
+  localStorage.setItem(
+    "taskItem",
+    document.getElementById("allTasks").innerHTML
+  );
+}
+
+function clearStorage() {
+  localStorage.clear();
+  window.location.reload();
+}
+
+document.getElementById("clear-all").onclick = () => {
+  clearStorage();
+};
+
+//---------------------------------------------------------------------//
 //DYNAMIC FAVICON FOR BOTH DARK THEME AND LIGHT THEME
-lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+lightSchemeIcon = document.querySelector("link#light-scheme-icon");
+darkSchemeIcon = document.querySelector("link#dark-scheme-icon");
 
 function onUpdate() {
   if (matcher.matches) {
@@ -74,7 +76,6 @@ function onUpdate() {
     darkSchemeIcon.remove();
   }
 }
-matcher = window.matchMedia('(prefers-color-scheme: dark)');
+matcher = window.matchMedia("(prefers-color-scheme: dark)");
 matcher.addListener(onUpdate);
 onUpdate();
-
